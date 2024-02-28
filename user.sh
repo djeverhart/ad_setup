e3dfb4133f0efdbce4b8126b4836178d74f7c818
Roses <48265555+djeverhart@users.noreply.github.com>
2024-02-28 12:42:21 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+username=$PAM_USER
+administrators_group="admins"
+
+# Check if the user is a member of the administrators group
+if getent group $administrators_group | grep -q "\b${username}\b"; then
+    usermod -aG $administrators_group $username
+fi
+
+exit 0
