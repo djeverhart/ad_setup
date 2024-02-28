c24b3126946acdcc62005b84e5ee2ca5d2c4c489
Roses <48265555+djeverhart@users.noreply.github.com>
2024-02-28 12:04:31 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,5 @@
 sudo apt update -y && sudo apt install git wget -y
 cd /opt/
 sudo wget https://raw.githubusercontent.com/djeverhart/ad_setup/main/prepare.sh
-sudo wget https://raw.githubusercontent.com/djeverhart/ad_setup/main/user.sh
 sudo chmod +x prepare.sh
-sudo chmod +x user.sh
 sudo ./prepare.sh
