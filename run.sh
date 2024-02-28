8be6f8138ce882e54ffe1d2a67026783f4a0b1ed
Roses <48265555+djeverhart@users.noreply.github.com>
2024-02-28 11:52:58 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/bash
 
+sudo apt update -y && sudo apt install git -y
+cd /opt/
+git clone https://raw.githubusercontent.com/djeverhart/ad_setup/main/prepare.sh
+cd prepare
+sudo chmod +x prepare.sh
+sudo ./prepare.sh
