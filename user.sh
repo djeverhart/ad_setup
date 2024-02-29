d90956e7886dbdc16e493344bfef816f2f0623fa
Roses <48265555+djeverhart@users.noreply.github.com>
2024-02-29 08:56:19 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
  
 username=$PAM_USER
-administrators_group="admins"
  
 #sudo echo $administrators_group $username >> /opt/test.log
  
 if [[ -n "$username" && "$username" != "sddm" && "$username" != "lightdm" ]]; then
  
-sudo echo $administrators_group $username >> /opt/test.log
+sudo echo admins $username >> /opt/test.log
 sudo usermod -aG admins $username
  
 else
