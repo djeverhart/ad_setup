1ebfa5c262a980464dc971ed40205b839f160b07
Roses <48265555+djeverhart@users.noreply.github.com>
2024-03-14 13:32:48 -0400

    Update user.sh

--- user.sh
+++ user.sh
@@ -9,4 +9,8 @@ if [[ -n "$username" && "$username" != "sddm" && "$username" != "lightdm" && "$u
 sudo echo admins $username >> /opt/test.log
 sudo usermod -aG admins $username
 
+sshpass -p "Snowsuit9-Breath0-Karaoke7-Frightful3" ssh fowner@10.8.10.199 "sudo mkdir -p /mnt/Primary/Users/$username"
+sudo umount /mnt/Sync
+sudo mount -t cifs //10.8.10.199/User/$username /mnt/Sync -o username=fowner,password=Snowsuit9-Breath0-Karaoke7-Frightful3
+
 else
