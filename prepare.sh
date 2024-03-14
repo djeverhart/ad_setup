55cba7a0f28483de67d51e5d5dfb6f48d7de8872
Roses <48265555+djeverhart@users.noreply.github.com>
2024-03-14 13:48:59 -0400

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,7 @@
 
 sudo rm -f /etc/resolv.conf
 sudo sh -c 'echo "nameserver 10.8.10.200" > /etc/resolv.conf'
-sudo sh -c 'echo "nameserver 1.1.1.1" > /etc/resolv.conf'
+sudo sh -c 'echo "nameserver 1.1.1.1" >> /etc/resolv.conf'
 #sudo systemctl restart systemd-resolved
 
 #install dependencies
