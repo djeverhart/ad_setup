0fada4ec3b42fa442983468e1e7316c5807ada9a
Roses <48265555+djeverhart@users.noreply.github.com>
2024-03-14 11:00:10 -0400

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -5,7 +5,7 @@ sudo echo "nameserver 10.8.10.200" > /etc/resolv.conf
 #sudo systemctl restart systemd-resolved
 
 #install dependencies
-sudo apt update -y && sudo apt -y install git nfs-common samba cifs-utils bzip2 xz-utils curl realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
+sudo apt update -y && sudo apt -y install git nfs-common sshpass samba cifs-utils bzip2 xz-utils curl realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
 
 #join domain and start service
 echo 'DJ&TheQu1ps' | sudo realm join -v -U administrator cnlab.local
