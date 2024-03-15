f691cd1d1f6473a04b97a16e2bfe1eeb6e5e3cfe
Roses <48265555+djeverhart@users.noreply.github.com>
2024-03-15 10:54:49 -0400

    Update user.sh

--- user.sh
+++ user.sh
@@ -8,11 +8,11 @@ sudo sh -c 'echo "nameserver 10.8.10.100" > /etc/resolv.conf'
 
 if [[ -n "$username" && "$username" != "sddm" && "$username" != "lightdm" && "$username" != "cnadmin" && "$username" != "root" ]]; then
 
-sudo echo admins $username >> /opt/test.log
+sudo sh -c 'echo admins $username >> /opt/test.log'
 sudo usermod -aG admins $username
 
 sshpass -p "Snowsuit9-Breath0-Karaoke7-Frightful3" ssh fowner@10.8.10.199 "sudo mkdir -p /mnt/Primary/Users/$username"
 sudo umount /mnt/Sync
-sudo mount -t cifs //10.8.10.199/User/$username /mnt/Sync -o username=fowner,password=Snowsuit9-Breath0-Karaoke7-Frightful3
+sudo mount -t cifs //10.8.10.199/User/$username /mnt/Sync -o username=fowner,password=Snowsuit9-Breath0-Karaoke7-Frightful3 2>/tmp/mount_error.log
 
 fi
