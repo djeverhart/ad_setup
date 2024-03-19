767b0c58da86d8a6173f7c055385106e48dba367
Roses <48265555+djeverhart@users.noreply.github.com>
2024-03-19 11:56:23 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 sudo apt update -y && sudo apt install git wget python3 python3-pip -y
-sudo pip3 install python-pam
 cd /opt/
 sudo wget https://raw.githubusercontent.com/djeverhart/ad_setup/main/compile.sh
 sudo chmod +x compile.sh
