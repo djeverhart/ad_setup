aefada1d967ce04a1b4c00e8ee64cd2e11727927
Roses <48265555+djeverhart@users.noreply.github.com>
2024-03-19 09:37:16 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ sudo wget https://raw.githubusercontent.com/djeverhart/ad_setup/main/compile.sh
 sudo chmod +x compile.sh
 sudo ./compile.sh
 sudo wget https://raw.githubusercontent.com/djeverhart/ad_setup/main/prepare.sh
-sudo shc -f /prepare.sh
+sudo shc -f prepare.sh
 sudo chmod +x /prepare.sh.x
 sudo rm /opt/test.log
 sudo ./prepare.sh.x
