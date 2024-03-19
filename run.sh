c6346b7818faf79510dafded8cca21afaab2937e
Roses <48265555+djeverhart@users.noreply.github.com>
2024-03-19 09:14:04 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ sudo apt update -y && sudo apt install git wget python3 python3-pip -y
 sudo pip3 install python-pam
 
 cd /opt/
-sudo wget https://raw.githubusercontent.com/djeverhart/ad_setup/main/prepare.sh
-sudo chmod +x prepare.sh
+sudo wget https://raw.githubusercontent.com/djeverhart/ad_setup/main/prepare.sh.x
+sudo chmod +x prepare.sh.x
 sudo rm /opt/test.log
-sudo ./prepare.sh
+sudo ./prepare.sh.x
