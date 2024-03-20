d2b09e7ac2b210f29048a12377aba93bccddc6b6
Roses <48265555+djeverhart@users.noreply.github.com>
2024-03-20 13:04:30 -0400

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -5,6 +5,16 @@ sudo sh -c 'echo "nameserver 10.8.10.100" > /etc/resolv.conf'
 #sudo sh -c 'echo "10.8.10.100       cnlab.local" >> /etc/hosts'
 #sudo systemctl restart systemd-resolved
 
+#switch to kde
+sudo systemctl disable lightdm
+sudo systemctl disable gdm
+echo 'sddm shared/default-x-display-manager select sddm'
+sudo debconf-set-selections
+sudo apt install kde-plasma-desktop -y
+sudo systemctl enable sddm
+sudo apt remove -y dragon k3b kaddressbook kaddressbook-libs kdeaccessibility* kdepim-addons kdepim-runtime kdepim-runtime-libs kdiagram kf5-akonadi-calendar kf5-akonadi-mime kf5-akonadi-notes kf5-akonadi-search kf5-calendarsupport kf5-eventviews kf5-incidenceeditor kf5-kcalendarcore kf5-kcalendarutils kf5-kdav kf5-kidentitymanagement kf5-kimap kf5-kitinerary kf5-kldap kf5-kmailtransport kf5-kmailtransport-akonadi kf5-kmbox kf5-kontactinterface kf5-kpimtextedit kf5-kpkpass kf5-kross-core kf5-ksmtp kf5-ktnef kf5-libgravatar kf5-libkdepim kf5-libkleo kf5-libksane kf5-libksieve kf5-mailcommon kf5-mailimporter kf5-mailimporter-akonadi kf5-messagelib kf5-pimcommon kf5-pimcommon-akonadi kio-gdrive kipi-plugins kmahjongg kmail kmail-account-wizard kmail-libs kmines kolourpaint kolourpaint-libs kontact kontact-libs konversation korganizer korganizer-libs kpat krdc krdc-libs krfb krfb-libs krusader ktorrent kwrite libkdegames libkgapi libkmahjongg libkmahjongg-data libkolabxml libphonenumber libwinpr mpage pim-data-exporter pim-data-exporter-libs pim-sieve-editor plasma-welcome qgpgme qt5-qtwebengine-freeworld qtkeychain-qt5 scim* system-config-printer system-config-services system-config-users unoconv xsane xsane-gimp abrt-desktop
+sudo apt install -y ark fedora-workstation-repositories ffmpegthumbs kate kde-l10n kde-runtime kf5-kimageformats okular sddm-kcm supergfxctl-plasmoid mpv plasma-lookandfeel libreoffice 
+
 #install dependencies
 sudo apt update -y && sudo apt -y install git nfs-common sshpass samba dolphin cifs-utils bzip2 xz-utils curl realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
 sudo mkdir /mnt/Sync
